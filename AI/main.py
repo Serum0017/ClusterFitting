d3e3f8474bc77a496d51b909e72f9815e1edcf5e
Serum0017 <96929271+Serum0017@users.noreply.github.com>
2024-02-12 14:04:01 -0800

    call python from js complete

---
+++ AI/main.py
@@ -0,0 +1,3 @@
+# testing function
+def magnificent_ai(parameter1):
+    return parameter1 * 2
--- index.js
+++ index.js
@@ -17,3 +17,14 @@ async function wait(delayInSeconds=1){
         setTimeout(resolve, delayInSeconds * 1000);
     })
 }
+
+// running python!
+const nodecallspython = require("node-calls-python");
+const py = nodecallspython.interpreter;
+
+py.import("./AI/main.py").then(async function(pymodule) {
+    const result = await py.call(pymodule, "magnificent_ai", 3);
+
+    // expected output: 6
+    console.log("I called python up. They said: ", {result});
+});
